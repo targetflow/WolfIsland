d93241140a0e33f73120724c25421bc78c8119a3
alex.orlovskyi <orlovskyi.alex@gmail.com>
2018-04-21 13:05:00 +0300

    Create controller class to operate the game/simulation

---
+++ controller/Controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex Orlovskyi on 21.04.18.
+//
+
+#include "Controller.h"
---
+++ controller/Controller.h
@@ -0,0 +1,14 @@
+//
+// Created by Alex Orlovskyi on 21.04.18.
+//
+
+#ifndef CPP_CONTROLLER_H
+#define CPP_CONTROLLER_H
+
+
+class Controller {
+
+};
+
+
+#endif //CPP_CONTROLLER_H
