78e1fc4d23ecb9677a10ab0794697599e1d253dd
Pkatharine <katya.pereverzeva2109@gmail.com>
2018-04-21 12:45:33 +0300

    class Field was replaced with Singletone

--- include/Field.h
+++ include/Field.h
@@ -5,12 +5,19 @@
 #ifndef CPP_FIELD_H
 #define CPP_FIELD_H
 #include "Cell.h"
+
+
 class Field{
+private:
+    Field(); // Disallow instantiation outside of the class.
 public:
+
+    static auto instance(){
+        static Field field;
+        return field;
+    }
     Cell * cells[400];
 protected:
-private:
-
-
 };
+
 #endif //CPP_FIELD_H
