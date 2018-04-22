8ac4dd8530113aa3bd1c33f003a9a2533f838d21
alex.orlovskyi <orlovskyi.alex@gmail.com>
2018-04-22 18:40:12 +0300

    Output matrix with all generated rabbits to console.

--- view/ConsoleView.cpp
+++ view/ConsoleView.cpp
@@ -10,15 +10,33 @@ ConsoleView::~ConsoleView() = default;
 
 void ConsoleView::printFieldToConsole(Field field) {
     std::string fieldView;
+    std::string cellContent;
+    unsigned long countOfRabbits;
     for (unsigned long i = 0; i < 400; i++)
     {
-        if ((i+1)%20 != 0)
-            fieldView += "[ ]";
+        //construct Cell contents
+        if(!field.getCells()->at(i).getRabbits()->empty())
+        {
+            countOfRabbits = field.getCells()->at(i).getRabbits()->size();
+            for (unsigned long j = 0; j < countOfRabbits; j++)
+            {
+                cellContent += "R";
+            }
+        }
         else
-            fieldView += "[ ]\n";
-//        if(!field.getCells()->at(i).getRabbits()->empty())
-//            std::cout << " There's 1+ rabbit in cell [" << field.getCells()->at(i).getCoordinates()[0] << ", " <<
-//                      field.getCells()->at(i).getCoordinates()[1] << "]." << std::endl;
+        {
+            cellContent = "";
+        }
+
+        //construct Field
+        fieldView.append("[");
+        fieldView.append(cellContent);
+        fieldView.append("] ");
+        if ((i+1)%20 == 0)
+            fieldView += "\n";
+
+        // clear-up interation vars
+        cellContent = "";
     }
     std::cout << fieldView << std::endl;
 }
