86aeb062cc16b98a7760076f7153a4fa238db4af
alex.orlovskyi <orlovskyi.alex@gmail.com>
2018-05-05 20:33:00 +0300

    Create GUI View skeleton

---
+++ view/GUIView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex Orlovskyi on 05.05.18.
+//
+
+#include "GUIView.h"
---
+++ view/GUIView.h
@@ -0,0 +1,12 @@
+//
+// Created by Alex Orlovskyi on 05.05.18.
+//
+
+#ifndef CPP_GUIVIEW_H
+#define CPP_GUIVIEW_H
+
+class GUIView {
+
+};
+
+#endif //CPP_GUIVIEW_H
