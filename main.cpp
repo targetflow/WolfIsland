74d95e1064b28de859c44054912dab07f6dc6512
Pkatharine <katya.pereverzeva2109@gmail.com>
2018-05-07 21:17:30 +0300

    now objects can move automate every 1 sec

--- main.cpp
+++ main.cpp
@@ -12,15 +12,17 @@ int main(){
     const int cOfFences = 5;
     const int countOfSteps = 3;
     const bool useGUI = true;
+    const float FPS = 60.0f;
 
     if (useGUI) {
         // SFML Program starts here
         RenderWindow window(sf::VideoMode(640, 640), windowTitle);
-        window.setFramerateLimit(60); // без цього комп іде на взрив, проц ппц
+        window.setFramerateLimit(FPS); // без цього комп іде на взрив, проц ппц
 
         Controller controller(nRabbits, nMWolves, nWWolves, cOfFences, &window);
 
         Time delayTime = sf::seconds(1); // без цієї затримки натиснення клавіші побуджує більше ніж 1 виклик.
+        sf::Clock clock;
 
         while (window.isOpen())
         {
@@ -30,9 +32,20 @@ int main(){
                 if (event.type == sf::Event::Closed)
                     window.close();
             }
+            if ((Keyboard::isKeyPressed(Keyboard::Right))){
+                if (clock.getElapsedTime().asSeconds() >= 60.0f / FPS)//здійснюєм рух коли ми на 60 кадрі
+                {
+                    controller.execute(1);
+                    clock.restart();
+                } else //нічо не робим поки знову не настане 60 кадр із 60-ти за секунду
+                {
+                    Time sleepTime = seconds((60.0f / FPS) - clock.getElapsedTime().asSeconds());
+                    sleep(sleepTime);
+                }
+            }
 
             if ((Keyboard::isKeyPressed(Keyboard::Left))) {
-                controller.execute(1);
+                controller.execute(1);// робить рух можливим
                 sf::sleep(delayTime);
             }
 
