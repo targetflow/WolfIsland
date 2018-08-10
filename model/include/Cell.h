17a9d919eb9e6c65635fa51192ff5db5e36cbb5e
alex.orlovskyi <orlovskyi.alex@gmail.com>
2018-08-10 13:29:02 +0300

    Add licenses and comments: Cell.h, CMakeLists

--- model/include/Cell.h
+++ model/include/Cell.h
@@ -1,15 +1,34 @@
-//
-// Created by katya on 20/04/18.
-//
-// This file contains information about Cell property - fence presence, describes what kind of Animals
-// can be in one Cell and vector with Cell coordinates.
+/**
+ * WolfIsland environment modeling project.
+ *
+ * Copyright 2018 by Kateryna Pereverzeva <katya.pereverzeva2109@gmail.com>
+ * Copyright 2018 by Oleksandr Orlovskyi <orlovskyi.alex@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ =========================================================================== */
+
 #ifndef CPP_CELL_H
 #define CPP_CELL_H
+
+#include <vector>
+
 #include "Wolf_W.h"
 #include "Wolf_M.h"
 #include "Rabbit.h"
-#include <vector>
 
+// This file contains information about Cell property - fence presence, describes what kind of Animals
+// can be in one Cell and vector with Cell coordinates.
 class Cell{
 private:
     std::vector <int> coordinates;
